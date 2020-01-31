5e54435dd27a97693f34c699cd6a4748f64f4d9e
Cedric <c.caudron59@gmail.com>
2020-01-31 13:20:05 +0100

    prepare administration interface

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\User;
+use App\Form\AdminRoleType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_show")
      */
-    public function index()
+    public function index(UserRepository $repo)
     {
+        $users = $repo->findAll();
+
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'users' => $users,
         ]);
     }
 }
