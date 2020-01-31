b450eed667b33f88b449fa7eb48314318b712552
Cedric <c.caudron59@gmail.com>
2020-01-31 15:10:01 +0100

    gestion droits users ok

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -16,9 +16,32 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin_show")
      */
-    public function index(UserRepository $repo)
+    public function index(UserRepository $repo, Request $request, EntityManagerInterface $manager)
     {
         $users = $repo->findAll();
+
+        if ($request->isMethod('post'))
+        {
+            $posts = $request->request->all();
+            dump($posts);
+
+            $user = $repo->find($request->request->get("idUser"));
+
+            $roles = [];
+            foreach ($posts as $key=>$post)
+            {
+                dump($key);
+                array_push($roles, $request->request->get($key));
+            }
+            dump($roles);
+
+            unset($roles[count($roles)-1]);
+            dump($roles);
+            $user->setRoles($roles);
+            $manager->persist($user);
+            $manager->flush();
+
+        }
 
 
         return $this->render('admin/index.html.twig', [
@@ -26,4 +49,5 @@ class AdminController extends AbstractController
             'users' => $users,
         ]);
     }
+
 }
